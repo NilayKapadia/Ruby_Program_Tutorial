9ee01f2e906ba758e63e8815077d94777467f339
NilayKapadia <nilayskapadia95@gmail.com>
2019-01-26 19:03:39 -0500

    Added the code template file

---
+++ ruby_intro_19spring.rb
@@ -0,0 +1,34 @@
+
+  # Part 1
+
+  def average(a)
+    # ADD YOUR CODE HERE
+  end
+
+  def twin_prime?(a,b)
+    # ADD YOUR CODE HERE
+  end
+
+  def check_product?(array, n)
+    # ADD YOUR CODE HERE
+  end
+
+# Part 2
+
+  def count_vowels(s)
+    # ADD YOUR CODE HERE
+  end
+
+  def valid_paretheses?(s)
+    # ADD YOUR CODE HERE
+  end
+
+  def longest_common_prefix(s)
+    # ADD YOUR CODE HERE
+  end
+
+# Part 3
+
+  class Student
+    # ADD YOUR CODE HERE
+  end
